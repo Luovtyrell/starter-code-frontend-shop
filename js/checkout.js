7ff4be56aadf238b22b08c50f972b5511472eff6
Luovtyrell <luciakjhgfds@gmail.com>
2024-05-11 10:42:39 +0200

    Added checkout form validation using checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -1,28 +1,61 @@
-
-// Exercise 6
 function validate() {
-	var error = 0;
-	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
+	let error = 0
+	const fields = ['fName', 'fLastN', 'fEmail', 'fPassword', 'fPhone', 'fAddress']
+	const abecedarioRegex = /^[a-zA-Z]+$/
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+	const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])/
 
-	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	
-	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
-		error++;
-	}
+	fields.forEach(formItem => {
+		const field = document.getElementById(formItem)
+		switch (formItem) {
+			case 'fName':
+			case 'fLastN':
+				if (!abecedarioRegex.test(field.value)) {
+					error++
+					field.classList.add('is-invalid')
+				} else {
+					field.classList.remove('is-invalid')
+				}
+				break;
+			case 'fEmail':
+				if (!emailRegex.test(field.value)) {
+					error++
+					field.classList.add('is-invalid')
+				} else {
+					field.classList.remove('is-invalid')
+				}
+				break;
+			case 'fPassword':
+				if (!passwordRegex.test(field.value)) {
+					error++
+					field.classList.add('is-invalid')
+				} else {
+					field.classList.remove('is-invalid')
+				}
+				break;
+			case 'fPhone':
+				if (isNaN(field.value) || field.value.length < 9) {
+					error++
+					field.classList.add('is-invalid')
+				} else {
+					field.classList.remove('is-invalid')
+				}
+				break;
+			default:
+				if (field.value === "" || field.value.length < 3) {
+					error++
+					field.classList.add("is-invalid")
+				} else {
+					field.classList.remove("is-invalid")
+				}
+		}
+	})
 
-	if(fEmail.value == ""){
-		error++;
+	if (error > 0) {
+		alert("Error")
+		return false
+	} else {
+		alert("OK")
+		return true
 	}
-	 
-	if(error>0){
-		alert("Error");
-	}else{
-		alert("OK");
-	}
-
 }
